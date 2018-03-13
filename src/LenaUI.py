f372e28ac03c87be3bd07f4db60e1bf5ba29cf6b
Kate Lovett <kea3k@mtmail.mtsu.edu>
2018-03-13 10:28:05 -0500

    Added UI and radio buton for A->(B->C). Logic needs to be implemented pending discussion & approval of using already existing functions to complete the operation. Also added in control.py a try and except for importing Tk. Recent update to python makes import statetments case-sesnsitive in such a way that the module was not being included and the program could not be run. Using try and except, it will be able to exceute regardless of what version the user has.

--- src/LenaUI.py
+++ src/LenaUI.py
@@ -25,6 +25,7 @@ MAC = 'Darwin'
 WINDOWS = 'Windows'
 AB = 'A_B'
 ABC = 'AB_C'
+ABC2 = 'A_BC'
 OK = 'ok'
 MAXTHREADS = 4
 codes = ('MAN','MAF','FAN','FAF','CHNSP','CHNNSP', \
@@ -217,6 +218,10 @@ class LenaUI:
         mid_type_label = ttk.Label(self.mid_frame, text='Type of Analysis')       
         self.mid_ab_btn = ttk.Radiobutton(self.mid_frame, text='A ---> B', variable=self.sequence_type, value=AB, command=disable_c)
         self.mid_abc_btn = ttk.Radiobutton(self.mid_frame, text='( A ---> B ) ---> C', variable=self.sequence_type, value=ABC, command=enable_c)        
+        #Added for FR-17 Logice need to be implemented: call A->B with (B) and (C), then call A->B again with
+        # (A) and (return from first call).
+        self.mid_abc2_btn = ttk.Radiobutton(self.mid_frame, text='A ---> ( B ---> C )', variable=self.sequence_type, value=ABC2, command=disable_c)
+
         mid_filler_label = ttk.Label(self.mid_frame, text="     ")
         mid_conf_label = ttk.Label(self.mid_frame, text="Configure Analysis")
         mid_conf_abc_a_label = ttk.Label(self.mid_frame, text="A") 
@@ -236,12 +241,13 @@ class LenaUI:
         mid_type_label.grid(row=0, column=0, columnspan=4)
         self.mid_ab_btn.grid(row=1, column=0, columnspan=3, sticky = W)
         self.mid_abc_btn.grid(row=2, column=0, columnspan=3, sticky = W)
-        mid_conf_abc_a_label.grid(row=3, column=0)
-        mid_conf_abc_b_label.grid(row=3, column=1)
-        mid_conf_abc_c_label.grid(row=3, column=2)
-        self.mid_abc_a_box.grid(row=4, column=0)
-        self.mid_abc_b_box.grid(row=4, column=1)
-        self.mid_abc_c_box.grid(row=4, column=2)
+        self.mid_abc2_btn.grid(row=3, column=0, columnspan=3, sticky = W)
+        mid_conf_abc_a_label.grid(row=4, column=0)
+        mid_conf_abc_b_label.grid(row=4, column=1)
+        mid_conf_abc_c_label.grid(row=4, column=2)
+        self.mid_abc_a_box.grid(row=5, column=0)
+        self.mid_abc_b_box.grid(row=5, column=1)
+        self.mid_abc_c_box.grid(row=5, column=2)
 
         mid_filler_label3.grid(row=5, column=0, columnspan=3)
         mid_pause_label.grid(row=6, column=0, columnspan=4, pady=5)
@@ -670,8 +676,10 @@ class LenaUI:
         # reset the selection to nothing selected update
         self.mid_ab_btn.configure(variable=self.sequence_type)
         self.mid_abc_btn.configure(variable=self.sequence_type)
+        #self.mid_abc2_btn.configure(variable=self.sequence_type)
         self.mid_ab_btn.update()
         self.mid_abc_btn.update()
+        #self.mid_abc2_btn.update()
 
         # reset slider and pause_duration entry box update
         self.mid_pause_slider.configure(variable=self.pause_duration)
